3f062ba8be698eaa0e08eb5d2b39753647a355c9
July <jul.ya@gmx.at>
2017-11-08 16:19:40 +0100

    image generator

---
+++ Assets/Scripts/ImageGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageGenerator : MonoBehaviour {
+
+	public Sprite[] images;
+	private bool[] used;
+
+	public GameObject[] targets;
+
+	public GameObject imagePrefab;
+			
+	public void RefreshImages(){
+		used = new bool[images.Length];
+
+		foreach (GameObject target in targets) {
+			
+			int random = Random.Range (0, images.Length);
+			while(used[random] != false){
+				random = Random.Range (0, images.Length);
+			}
+
+			GameObject go = Instantiate (imagePrefab, target.transform);
+			go.GetComponent<SpriteRenderer> ().sprite = images [random];
+			used [random] = true;
+		}
+
+		Debug.Log ("Restarted!");
+	}
+}
