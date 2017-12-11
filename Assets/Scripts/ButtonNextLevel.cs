474d50747eb9964b50063670f85a10e7b0d0a5b6
woifi90 <wolfi.kerni@gmail.com>
2017-12-11 10:50:41 +0100

    test App

---
+++ Assets/Scripts/ButtonNextLevel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonNextLevel : MonoBehaviour
+{ 
+
+    void Start()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        var se = new InputField.SubmitEvent();
+        se.AddListener(SubmitName);
+        input.onEndEdit = se;
+
+        //or simply use the line below, 
+        //input.onEndEdit.AddListener(SubmitName);  // This also works
+    }
+
+    private void SubmitName(string arg0)
+    {
+        if(arg0 == "Pizza")
+        {
+            var tex = GameObject.FindGameObjectWithTag("fun").GetComponent<Text>();
+            tex.text = "00";
+
+        }
+    }
+}
